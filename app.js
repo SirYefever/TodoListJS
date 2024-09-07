a5b7d5016ebe4e63a148d8e1d8a4bd690a0e9dc9
Xee <ninjaxxxflame@gmail.com>
2024-09-07 13:45:39 +0700

    Bugfix, progress bar upgraded with encouraging messages.

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ function addTodoManually() {
 function addTodo(name, done){
 	todoIterator++;
 	todoCounter++;
+	if (document.getElementById("noTasksMessage") != null) {
+		removeNoTasksMessage();
+	}
 	let todo = new Todo(name, done);
 	todoList[todoList.length] = todo;
 	let constructSpan = function() {
@@ -60,6 +63,7 @@ function addTodo(name, done){
 				    constructRedactButton() + constructDeleteButton() + "</li>";
 	}
 	let li = constructLi();
+	todoUL = document.getElementById("todoUL");
 	todoUL.insertAdjacentHTML("beforeend", li);
 	updateProgressBar();
 }
@@ -73,7 +77,7 @@ function redact(todoNumber) {
 		spanElementToRename.textContent = newName;
 	}
 }
-
+// Fix the bug that confuses todoCounter and todoNumber or something like that.
 function manageCheckboxing(todoNumber) {
 	for (let i = 0; i < todoList.length; i++){
 		if (todoNumber === todoList[i].number) {
@@ -103,6 +107,9 @@ function remove(todoNumber) {
 	}
 	updateProgressBar();
 	todoUL.removeChild(document.getElementById("li" + todoNumber.toString()));
+	if (todoCounter === 0) {
+		displayNoTasksMessage();
+	}
 }
 
 function invokeImportWindow() {
@@ -113,6 +120,7 @@ function parseInputFile() {
 	let reader = new FileReader();
 	reader.readAsText(importInput.files[0]);
 	reader.onload = function() {
+		console.log("ONLOAD FIRED");
 		var todoArray = JSON.parse(reader.result);
 		for (let i = 0; i < todoArray.length; i++) { 
 			addTodo(todoArray[i].name, todoArray[i].done);
@@ -147,8 +155,14 @@ function download(data, filename, type) {
 
 function updateProgressBar() {
 	let progressBar = document.getElementById("progressBar");
+	if (todoCounter === 0) {
+		progressBar.value = 0;
+		displayEncouragingMessage();
+		return;
+	}
 	let percentage = doneCounter / todoCounter;
 	progressBar.value = percentage;
+	displayEncouragingMessage();
 }
 
 let todoUL = document.getElementById("todoUL");
@@ -163,3 +177,44 @@ importInput.addEventListener("change", () => {
 	}
 })
 
+let ulDivForTasks = document.getElementById("todoListDiv");
+let noTaskMessage = document.getElementById("noTasksMessage");
+
+function displayNoTasksMessage() {
+	todoUL = document.getElementById("todoUL");
+	ulDivForTasks = document.getElementById("todoListDiv");
+	ulDivForTasks.removeChild(todoUL);
+	const noTasksMessage = "<p id=\"noTasksMessage\">It's time to add some new tasks!</p>";
+	ulDivForTasks.insertAdjacentHTML("beforeend", noTasksMessage);
+}
+
+function removeNoTasksMessage() {
+	todoUL = document.getElementById("todoUL");
+	ulDivForTasks = document.getElementById("todoListDiv");
+	noTaskMessage = document.getElementById("noTasksMessage");
+	ulDivForTasks.removeChild(noTaskMessage);
+	//const ulElement = documnet.createElement("ul");
+	const ulElement = "<ul id=\"todoUL\"></ul>";
+	//ulDivForTasks.appendChild(ulElement);
+	ulDivForTasks.insertAdjacentHTML("beforeend", ulElement);
+}
+
+function displayEncouragingMessage() {
+	let progressDiv = document.getElementById("progressDiv");
+	let messageContents;
+	if (doneCounter === todoCounter && todoCounter != 0) {
+		if (todoCounter < 4 && todoCounter > 1) {
+			messageContents = "Good job completing " + todoCounter.toString() + " tasks!";
+		} else {
+			messageContents = "You've done all your tasks! Great job!";
+		}
+	}
+	else if (doneCounter === Math.ceil(todoCounter / 2) && todoCounter != 0) {
+		messageContents = "Half-way through! Keep it up!";
+	}else {
+		messageContents = "";
+	}
+	let message = "<p id=\"congratulationsMessage\">" + messageContents + "</p>";
+	let messageElement = document.getElementById("encouragingMessage");
+	messageElement.innerHTML = message;
+}
